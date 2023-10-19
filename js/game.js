dda754d12c510359f74df1ebd039c8194c4673d4
maksidze <maksim_sokolovskij@mail.ru>
2023-10-19 10:07:39 +0600

    Player animation

--- js/game.js
+++ js/game.js
@@ -3,7 +3,7 @@ let ids = ["r_01", "r_02", "r_03", "r_04", "r_05", "r_06", "r_07", "r_08", "r_09
     "r_11", "r_12", "r_13", "r_14", "r_15", "r_16", "r_17", "r_18", "r_19", "r_20", "r_21",
     "r_22", "r_23", "r_24", "r_25", "r_26", "r_27", "r_28", "r_29", "r_30", "r_31", "r_32"];
 const MAP_MAX = ids.length - 1;
-const ANIMATION_PLAYER_STEPS = 200;
+const ANIMATION_PLAYER_STEPS = 20;
 
 let players = []
 
@@ -19,6 +19,7 @@ let cellSize;
 let suCellSize;
 
 let movePlayerAnimation = {"enabled": false, "from": null, "to": null, "count": 0}
+let playerPosition = "r_01_0";
 
 function getNextPos() {
     pos = pos < MAP_MAX ? pos + 1 : 0;
@@ -26,32 +27,40 @@ function getNextPos() {
 
 function animateStep(count = 0) {
     if (!count) {
+        console.log(stepCount);
+
+        if (movePlayerAnimation.enabled) {
+            animatePlayerStep();
+            return;
+        }
         if (stepCount) {
-            document.getElementById(ids[pos]).classList.remove("show");
+            stepCount--;
+            //document.getElementById(ids[pos]).classList.remove("show");
             getNextPos();
-            document.getElementById(ids[pos]).classList.add("show");
+            //document.getElementById(ids[pos]).classList.add("show");
+            console.log("Target: " + getFreeSubCell(document.getElementById(ids[pos])));
+            startAnimatePlayer(playerPosition, getFreeSubCell(document.getElementById(ids[pos])));
         } else {
             clearInterval(timerStep);
         }
-        stepCount--;
     } else {
         console.log(count);
         stepCount = count;
-        timerStep = setInterval(animateStep, 250);
+        timerStep = setInterval(animateStep, 20);
     }
 }
 
 function getFreeSubCell(cell) {
     for (let i = 0; i < cell.childNodes.length; i++) {
-        if (cell.childNodes[i].innerHTML === "") return cell.childNodes[i];
+        if (cell.childNodes[i].innerHTML === "") return cell.childNodes[i].id;
     }
     return false;
 }
 
-function animate1() {
+function startAnimatePlayer(from_id, to_id) {
     //elPlayer.style.width =
-    movePlayerAnimation.from = document.getElementById("r_01_1");
-    movePlayerAnimation.to = document.getElementById("r_18_2");
+    movePlayerAnimation.from = document.getElementById(from_id);
+    movePlayerAnimation.to = document.getElementById(to_id);
     movePlayerAnimation.count = ANIMATION_PLAYER_STEPS;
     movePlayerAnimation.enabled = true;
     elPlayer.appendChild(movePlayerAnimation.from.childNodes[0]);
@@ -64,19 +73,36 @@ function animate1() {
     console.log(elPlayer);
 }
 
-function animateStep1() {
-    console.log(movePlayerAnimation);
+// https://easings.net/
+function easeInOutExpo(x) {
+    return x === 0
+        ? 0
+        : x === 1
+            ? 1
+            : x < 0.5 ? Math.pow(2, 20 * x - 10) / 2
+                : (2 - Math.pow(2, -20 * x + 10)) / 2;
+
+}
+
+function linear(x) {
+    return x;
+}
+
+function animatePlayerStep(decrement = true) {
+    //console.log(movePlayerAnimation);
     if (!movePlayerAnimation.enabled) return;
     let start = movePlayerAnimation.from.getBoundingClientRect();
     let end = movePlayerAnimation.to.getBoundingClientRect();
 
-    console.log("Count: " + movePlayerAnimation.count);
-    console.log(elPlayer);
-    let pos_x = start.left + Math.floor((end.left - start.left) / ANIMATION_PLAYER_STEPS * (ANIMATION_PLAYER_STEPS - movePlayerAnimation.count));
-    let pos_y = start.top + Math.floor((end.top - start.top) / ANIMATION_PLAYER_STEPS * (ANIMATION_PLAYER_STEPS - movePlayerAnimation.count));
-    movePlayerAnimation.count--;
+    //console.log("Count: " + movePlayerAnimation.count);
+    //console.log(elPlayer);
+    let pos_x = window.scrollX + start.left + Math.floor((end.left - start.left) * easeInOutExpo((ANIMATION_PLAYER_STEPS - movePlayerAnimation.count) / ANIMATION_PLAYER_STEPS));
+    let pos_y = window.scrollY + start.top + Math.floor((end.top - start.top) * easeInOutExpo((ANIMATION_PLAYER_STEPS - movePlayerAnimation.count) / ANIMATION_PLAYER_STEPS));
+    if (decrement) movePlayerAnimation.count--;
     if (movePlayerAnimation.count < 0) {
         movePlayerAnimation.enabled = false;
+
+        playerPosition = movePlayerAnimation.to.id;
 
         movePlayerAnimation.to.appendChild(elPlayer.children[0]);
         elPlayer.innerHTML = "";
@@ -84,20 +110,20 @@ function animateStep1() {
         return;
     }
 
-    console.log("end: " + end.left);
-    console.log("pos_x: " + pos_x);
+    //console.log("end: " + end.left);
+    //console.log("pos_x: " + pos_x);
 
     elPlayer.style.left = pos_x + "px";
     elPlayer.style.top = pos_y + "px";
 }
 
 function aaa() {
-    timerStep = setInterval(animateStep1, 10);
+    timerStep = setInterval(animatePlayerStep, 100);
 }
 
 function test() {
-    //animateStep(Math.floor(Math.random() * 7));
-    animateStep();
+    animateStep(Math.floor(Math.random() * 6) + 1);
+    //animateStep();
 }
 
 
@@ -110,7 +136,7 @@ window.onload = (event) => {
 
     for (let cell of ids) {
         let el = document.getElementById(cell);
-        for (let i = 0; i < 8; i++) {
+        for (let i = 0; i < 4; i++) {
             let newEl = document.createElement("div");
             newEl.id = `${cell}_${i}`;
             el.appendChild(newEl);
@@ -119,15 +145,10 @@ window.onload = (event) => {
 
     let mp = document.createElement("div");
     mp.classList.add("player")
-    document.getElementById("r_01_1").appendChild(mp);
-    //animate(mp, document.getElementById("r_01_1"));
+    document.getElementById("r_01_0").appendChild(mp);
+
 }
 
 onresize = (event) => {
-    let sizes = movePlayerAnimation.from.getBoundingClientRect();
-    console.log(sizes);
-    elPlayer.style.width = sizes.width + "px";
-    elPlayer.style.height = sizes.height + "px";
-    elPlayer.style.left = sizes.left + "px";
-    elPlayer.style.top = sizes.top + "px";
+    animatePlayerStep(false);
 };
